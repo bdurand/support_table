b8120baa5b37933794e24fac9e30e171d2385f3d
Brian Durand <bbdurand@gmail.com>
2026-08-09 10:01:32 -0700

    update ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
-require "bundler/setup"
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
+
+require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
 
 require "logger"
 require "active_record"
+
+begin
+  require "simplecov"
+  SimpleCov.start do
+    skip ["/spec/"]
+  end
+rescue LoadError
+end
+
+Bundler.require(:default, :test)
 
 ActiveRecord::Base.establish_connection("adapter" => "sqlite3", "database" => ":memory:")
 
